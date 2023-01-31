91fd927d55593a3c3bf594048145578412e79448
m <m@kali.Home>
2023-02-01 00:49:17 +0200

    2nd commit

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 
 const app=express();
-app.use(express.static("build"));
+app.use("/",express.static(__dirname+"/build/"));
 
-app.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/build/index.js")
-})
+
 app.get("/api/cv",(req,res)=>{
     res.download(__dirname+"/Mohammed_Ayyad_CV.pdf");
+})
+app.get("/*",(req,res)=>{
+    res.sendFile(__dirname+"/build/index.html")
 })
 
 app.listen(3000);
